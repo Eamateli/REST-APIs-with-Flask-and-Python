8d5c3815dd8a210da350f6198595d435b3b461c6
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-09-02 19:10:46 +0300

    Adding the item Blueprint

--- resources/store.py
+++ resources/store.py
@@ -23,24 +23,24 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="Store not found.")
             
-    @blp.route("/store")
-    class StoreList(MethodView):
-        def get(self):
-            return {"stores": list(stores.values())}
+@blp.route("/store")
+class StoreList(MethodView):
+    def get(self):
+        return {"stores": list(stores.values())}
         
-        def post(self):
-            store_data = request.get_json()
-            if "name" not in store_data:
-                abort(
-                    400,
-                    message="Bad requets. Ensure 'name' is included in the JSON payload."
-                )
-            for store in stores.values():
-                if store_data["name"] == store["name"]:
-                    abort(400, message=f"Store already exists.")
+    def post(self):
+        store_data = request.get_json()
+        if "name" not in store_data:
+            abort(
+                 400,
+                message="Bad requets. Ensure 'name' is included in the JSON payload."
+            )
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message=f"Store already exists.")
                     
-            store_id = uuid.uuid4().hex
-            store = {**store_data, "id":store_id}
-            stores[store_id] = store
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id":store_id}
+        stores[store_id] = store
             
-            return store    
+        return store    
