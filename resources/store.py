a6b45565f5bb20e27fd7bbde66b77a4366250e3a
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-09-03 15:59:44 +0300

    Updating the blueprint

--- resources/store.py
+++ resources/store.py
@@ -3,6 +3,8 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
+
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
@@ -25,13 +27,10 @@ class Store(MethodView):
 class StoreList(MethodView):
     def get(self):
         return {"stores": list(stores.values())}
-        
-    def post(self):
-        store_data = request.get_json()
-        
-        if "name" not in store_data:
-            abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
-        
+    
+    
+    @blp.arguments(StoreSchema) 
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message="Store with the same name already exists.")
