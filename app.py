3db497fa1684f5986cda7c204e2e36cd5cab8adb
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-01-08 23:04:06 +0200

    Adding db URI

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
 from db import db
@@ -5,7 +7,7 @@ import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
 
 
@@ -16,6 +18,7 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
  
 
 
