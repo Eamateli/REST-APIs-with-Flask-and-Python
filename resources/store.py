6cc62258b40f34cbda0aaa6871e9294ff8eb4ace
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-01-09 23:51:37 +0200

    Updating the store resource post method and importing

--- resources/store.py
+++ resources/store.py
@@ -2,6 +2,9 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
 
@@ -33,12 +36,13 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema) 
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message="Store with the same name already exists.")
-                    
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-            
-        return store, 201
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except IndexError:
+            abort(400, message="A store with that name already exists.",)
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while  inserting the item.") 
+        
+        return store
