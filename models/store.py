2894ef5c46e83f2c174add116ba652b433673223
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-04-07 15:09:18 +0300

    Small update on the store model

--- models/store.py
+++ models/store.py
@@ -7,3 +7,4 @@ class StoreModel(db.Model):
     name = db.Column(db.String(88), unique=True, nullable=False)
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
     tag = db.relationship("TagModel", back_populates="store", lazy="dynamic")
+    
