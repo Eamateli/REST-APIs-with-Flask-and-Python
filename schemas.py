5a397a7b229ea78b63a7e851ba28331713b6e618
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-04-07 15:14:29 +0300

    Adding tag to the StoreSchema

--- schemas.py
+++ schemas.py
@@ -31,3 +31,5 @@ class ItemSchema(PlainItemSchema):
     
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+    
