8edcf61b01d547fd9d384edb5e248452e0b694fa
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-04-15 15:19:55 +0300

    Fixing bug

--- models/item_tag.py
+++ models/item_tag.py
@@ -4,6 +4,6 @@ class ItemTags(db.Model):
     __tablename__ = "items_tag"
     
     id = db.Column(db.Integer, primary_key=True)
-    item_id = db.Colum(db.Integer, db.ForeignKey("items.id"))
-    tag_id = db.Colum(db.Integer, db.ForeignKey("tag.id"))
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"))
     
