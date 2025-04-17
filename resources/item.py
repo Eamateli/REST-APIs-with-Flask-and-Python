f5e4aea4d2d72a171eb85aa5d082151c3b6b1e4b
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-04-17 15:20:35 +0300

    Adding the decorators for requiring a JWT

--- resources/item.py
+++ resources/item.py
@@ -10,11 +10,13 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item=ItemModel.query.get_or_404(item_id)
         return item
     
+    @jwt_required()
     def delete(self, item_id):
         item=ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -34,6 +36,7 @@ class Item(MethodView):
             
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
