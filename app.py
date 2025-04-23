9de652a5060e74e83d762c0db4846562da552fb3
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2025-04-23 16:13:53 +0300

    Importing and adding migrate. Removing unnecessary details

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
-import secrets
+
 
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
-import models
+
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -30,7 +31,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
 
 
     api = Api(app)
@@ -89,8 +90,6 @@ def create_app(db_url=None):
             401,
         )
     
-    with app.app_context():
-        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
